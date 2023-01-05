a8d219ef1e4d69bbb16f0493d853f3a220fc11e1
TonyH98 <tonyhoang@pursuit.org>
2023-01-05 10:39:47 -0500

    All react routes work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Home';
 import Transactions from './Components/transactions';
 import TransactionDetails from './Components/transactionDetails';
 import TransactionNew from './Components/transactionNew';
+import TransactionEdit from './Components/transactionEdit';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Route path="transaction" element={<Transactions/>}/>
             <Route path="/transaction/:index" element={<TransactionDetails/>}/>
             <Route path="/transaction/new" element={<TransactionNew/>}/>
+            <Route path="/transaction/:index/edit" element={<TransactionEdit/>}/>
             <Route path="*"/>
           </Routes>
         </main>
---
+++ src/Components/transactionEdit.js
@@ -0,0 +1,98 @@
+import { useState , useEffect} from "react";
+import { useParams, useNavigate , Link } from "react-router-dom";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL
+
+function TransactionEdit(){
+    let navigate = useNavigate()
+    let { index } = useParams();
+
+    const [transaction, setTransaction] = useState({
+      item_name: "",
+      amount: "",
+      date: "",
+      category: "",
+    });
+
+    const handleTextChange = (event) => {
+        setTransaction({ ...transaction, [event.target.id]: event.target.value });
+      };
+
+      useEffect(() => {
+        axios.get(`${API}/transaction/${index}`)
+        .then(res => setTransaction(res.data))
+        .catch(err => console.log(err))
+      }, [index]);
+
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.put(`${API}/transaction/${index}` , transaction)
+        .then((res) => {
+            setTransaction(res.data)
+            navigate(`/transaction/${index}`)
+        })
+        .catch(err => console.log(err))
+       
+      };
+
+      return(
+        <div className="New">
+        <form onSubmit={handleSubmit}>
+          <br></br>
+          <label htmlFor="item_name">Item Name:</label>
+          <input
+            id="item_name"
+            value={transaction.item_name}
+            type="text"
+            onChange={handleTextChange}
+            required
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="amount">Amount:</label>
+          <input
+            id="amount"
+            type="text"
+            required
+            value={transaction.amount}
+            onChange={handleTextChange}
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="date">Date:</label>
+          <input
+            id="date"
+            type="text"
+            value={transaction.date}
+            onChange={handleTextChange}
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="category">Category:</label>
+          <input
+            id="category"
+            type="text"
+            value={transaction.category}
+            onChange={handleTextChange}
+          />
+          <br></br>
+          <br></br>
+        <br></br>
+          <br></br>
+          <div className="new-buttons">
+          <input type="submit" />
+
+          <Link to={"/transaction"}>
+            <button className="cancel">Cancel</button>
+          </Link>
+
+          </div>
+        </form>
+      </div>
+      )
+
+}
+
+export default TransactionEdit
